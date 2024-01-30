fb1be8145fb5646a05159e6859e0cf0fb75d6f15
WoongE <woong17@naver.com>
2024-01-30 17:11:35 +0900

    type 명시

--- index.js
+++ index.js
@@ -5,40 +5,37 @@ const Base64 = require("crypto-js/enc-base64");
 
 const CLOUD_EDGE_API_BASE_URL = "https://edge.apigw.ntruss.com"
 
-async function purge() {
+function buildHeaders(path, method): Record<string, string> {
     const ncpAccessKeyId = core.getInput('ncp-access-key-id');
     const ncpSecretKey = core.getInput('ncp-secret-key');
+
+    const timestamp = Date.now();
+    const signature = Base64.stringify(hmacSHA256(`${method} ${path}\n${timestamp}\n${ncpAccessKeyId}`, ncpSecretKey));
+
+    return {
+        'Content-Type': 'application/json',
+        'x-ncp-apigw-signature-v2': signature,
+        'x-ncp-apigw-timestamp': timestamp,
+        'x-ncp-iam-access-key': ncpAccessKeyId
+    }
+}
+
+async function purge(): Promise<Array<number>> {
     const edgeProfileId = core.getInput('edge-profile-id');
     const edgeId = core.getInput('edge-id');
     const path = '/api/v1/profiles';
+    const method = 'GET';
 
-    // build signature
-    const timestamp = Date.now();
-    const signature = Base64.stringify(hmacSHA256(`GET ${path}\n${timestamp}\n${ncpAccessKeyId}`, ncpSecretKey));
-    const data = await fetch(CLOUD_EDGE_API_BASE_URL + path, {
-        headers: {
-            'Content-Type': 'application/json',
-            'x-ncp-apigw-signature-v2': signature,
-            'x-ncp-apigw-timestamp': timestamp,
-            'x-ncp-iam-access-key': ncpAccessKeyId
-        },
-        method: 'GET'
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-            return data;
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-            return error;
-        });
-    console.log('data: ', data);
     console.log('edge-profile-id: ', edgeProfileId);
     console.log('edge-id: ', edgeId);
 
-    const purgeRequestIds = [1, 2, 3, 4]; // TODO: Call NCP API to purge cache
-    return purgeRequestIds
+    // build signature
+    const headers = buildHeaders(path, method);
+    const response = await fetch(CLOUD_EDGE_API_BASE_URL + path, {headers, method});
+    const data = await response.json();
+    console.log('data: ', data);
+
+    return Array<number>(data.result)
 }
 
 purge()
